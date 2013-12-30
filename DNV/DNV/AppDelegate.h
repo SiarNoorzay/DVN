935432520c164e027b01ef8c8cd996390233db03
SiarNoorzay <siarnoorzay@gmail.com>
2013-12-30 11:22:59 -0500

    Created project

---
+++ DNV/DNV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DNV
+//
+//  Created by USI on 12/30/13.
+//  Copyright (c) 2013 USI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DNV/DNV/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DNV
+//
+//  Created by USI on 12/30/13.
+//  Copyright (c) 2013 USI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
