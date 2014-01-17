24fb7ebb419be524835b1876c27063b606484b2c
SiarNoorzay <siarnoorzay@gmail.com>
2014-01-17 10:17:12 -0500

    added calculator to dashboard

---
+++ DNV/DNV/CalculatorViewController.h
@@ -0,0 +1,58 @@
+//
+//  CalculatorViewController.h
+//  DNV
+//
+//  Created by USI on 1/17/14.
+//  Copyright (c) 2014 USI. All rights reserved.
+//
+
+
+
+#import <UIKit/UIKit.h>
+
+// We store the various buttons in a custom enumerator
+
+typedef enum {
+    kButtonMC, // 0
+    kButtonMPlus, // 1
+    kButtonMMinus, // 2
+    kButtonMR, // etc
+    
+    kButtonAC,
+    kButtonChangeSign,
+    kButtonDivide,
+    kButtonMultiply,
+    
+    kButtonSeven,
+    kButtonEight,
+    kButtonNine,
+    kButtonSubtract,
+    
+    kButtonFour,
+    kButtonFive,
+    kButtonSix,
+    kButtonAdd,
+    
+    kButtonOne,
+    kButtonTwo,
+    kButtonThree,
+    
+    kButtonZero,
+    kButtonDot,
+    kButtonEqual
+} kButton;
+
+@interface CalculatorViewController : UIViewController
+{
+    NSNumber *leftOperator;
+    kButton operation;
+    BOOL deleteInput;
+    double memory;
+}
+
+@property (nonatomic, retain) UILabel *resultLabel;
+@property (nonatomic, retain) NSMutableString *resultText;
+
+- (void)buttonPressed:(id)sender;
+
+@end
